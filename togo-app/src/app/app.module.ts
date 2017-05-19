79c67ba130a7b10a366b2497a404e316051d29cb
Andreas Karpasitis <karpas@Andreass-MacBook-Pro.local>
2017-05-19 20:55:36 +0200

    milligram style and bootstrap add plus initial form implemented

--- togo-app/src/app/app.module.ts
+++ togo-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MainFormComponent } from './main-form/main-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainFormComponent
   ],
   imports: [
     BrowserModule,
